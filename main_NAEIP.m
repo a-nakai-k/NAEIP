0c7bef1b179d9d4f24e3d4a71e913c156a5dc2d6
a-nakai-k <nakai.ayano@sys.i.kyoto-u.ac.jp>
2021-05-02 00:14:10 +0900

    added codes for NAEIP

---
+++ main_NAEIP.m
@@ -0,0 +1,135 @@
+%----------------------------
+% NAE-IP
+% using synthetic data
+%----------------------------
+% by Ayano NAKAI-KASAI (nakai.ayano@sys.i.kyoto-u.ac.jp)
+
+clear;
+close all;
+
+%--- Set path to GPML Toolbox ---%
+% addpath(genpath('~~~'));
+
+%--- Parameters for data settings ---%
+ntrain = 10^4;              % number of training data points
+dtrain = 1;                 % dimension of training data
+ntest = ntrain*10^(-2);     % number of test data
+dtest = 1;                  % dimension of test data
+noisevar = 0.04;            % additive noise ~ Gaussian(0,noisevar)
+rtrain = 4;                 % range of training data
+rtest = 5;                  % range of test data
+nIteration = 10;            % number of iteration
+%--- Parameters for NAE-IP ---%
+ni = 500;                   % number of subdata per each expert
+p = ntrain/ni;              % number of experts
+nt = 50;                    % number of test inputs processed at once
+scale = 1.5;                % scaling factor for sketching dimension, nu=scale*nipt
+f = @(x) sinc(x);
+%--- Initialization ---%
+t_NAEIPbt = zeros(nIteration,1);
+mse_NAEIPbt = zeros(nIteration,1); msll_NAEIPbt = zeros(nIteration,1);
+t_NAEIPbtot = zeros(nIteration,1);
+mse_NAEIPbtot = zeros(nIteration,1); msll_NAEIPbtot = zeros(nIteration,1);
+t_NAEIPbtur = zeros(nIteration,1);
+mse_NAEIPbtur = zeros(nIteration,1); msll_NAEIPbtur = zeros(nIteration,1);
+t_NAEIPat = zeros(nIteration,1);
+mse_NAEIPat = zeros(nIteration,1); msll_NAEIPat = zeros(nIteration,1);
+t_NAEIPur = zeros(nIteration,1);
+mse_NAEIPur = zeros(nIteration,1); msll_NAEIPur = zeros(nIteration,1);
+
+for itr = 1:nIteration
+    disp(['Iteration = ' num2str(itr)]);
+    %--- Generation of training and test data ---%
+    xtrain = linspace(-rtrain,rtrain,ntrain)';                  % training samples in [-rtrain,rtrain]
+    ztrain = f(xtrain) + sqrt(noisevar).*randn(ntrain,dtrain);  % observations
+    xtest = -rtest + 2*rtest*rand(ntest,dtest);                 % test samples in [-ranges,ranges]
+    xtest = sort(xtest,'ascend');                               % for simplicity, transform xtest to ascend-ordered
+    ztest = f(xtest) + sqrt(noisevar).*randn(ntest,dtest);      % targets
+    %- normalization
+    xtrain_mean = mean(xtrain); 
+    xtrain_std  = std(xtrain);
+    xtrain = (xtrain-repmat(xtrain_mean,ntrain,1)) ./ repmat(xtrain_std,ntrain,1) ;    
+    ztrain_mean = mean(ztrain);
+    ztrain_std  = std(ztrain);
+    ztrain = (ztrain-ztrain_mean)/ztrain_std;
+    xtest = (xtest - repmat(xtrain_mean,ntest,1)) ./ (repmat(xtrain_std,ntest,1));
+    
+    %- model parameters
+    sf = 0.25;
+    ell = 0.8;
+    sn = 0.5; 
+    meanfunc = [];
+    covfunc = @covSEiso;                % squared exponential covariance function
+    likfunc = @likGauss;                % Gaussian likelihood
+    inffunc = @infGaussLik;
+    hyp = struct('mean', meanfunc, 'cov', [log(ell) log(sf)], 'lik', log(sn));
+    
+    %- sub-model construction
+    models = cell(1,p);
+    Idx = randperm(ntrain);             % random partition
+    for i = 1:p
+        Idxi = Idx((i-1)*ni+1:i*ni);
+        models{i}.Xi = xtrain(Idxi);
+        models{i}.Zi = ztrain(Idxi);
+        models{i}.Ximean = mean(models{i}.Xi);
+        models{i}.hyp = hyp;
+        models{i}.meanfunc = meanfunc;
+        models{i}.likfunc = likfunc;
+        models{i}.covfunc = covfunc;
+        models{i}.inffunc = inffunc;
+    end
+    
+    criterion = 'NAEIPbt' ; % NAEIPbt, NAEIPbtot, NAEIPbtur, NAEIPat, NAEIPur
+    [mu_NAEIPbt,s2_NAEIPbt,t_NAEIPbt(itr)] = prediction(xtest,models,criterion,nt);
+    mu_NAEIPbt = mu_NAEIPbt*ztrain_std + ztrain_mean;
+    s2_NAEIPbt = s2_NAEIPbt*ztrain_std^2;
+    [mse_NAEIPbt(itr),msll_NAEIPbt(itr)] = Results(ztest,mu_NAEIPbt,s2_NAEIPbt);
+    disp(['NAEIPbt: ' num2str(mse_NAEIPbt(itr)) ', ' num2str(msll_NAEIPbt(itr)) ', ' num2str(t_NAEIPbt(itr))]);
+    
+    criterion = 'NAEIPbtot' ; % NAEIPbt, NAEIPbtot, NAEIPbtur, NAEIPat, NAEIPur
+    nip = scale*nt - nt;            % number of inducing points except for test inputs
+    [mu_NAEIPbtot,s2_NAEIPbtot,t_NAEIPbtot(itr)] = prediction(xtest,models,criterion,nt,nip);
+    mu_NAEIPbtot = mu_NAEIPbtot*ztrain_std + ztrain_mean;
+    s2_NAEIPbtot = s2_NAEIPbtot*ztrain_std^2;
+    [mse_NAEIPbtot(itr),msll_NAEIPbtot(itr)] = Results(ztest,mu_NAEIPbtot,s2_NAEIPbtot);
+    disp(['NAEIPbtot: ' num2str(mse_NAEIPbtot(itr)) ', ' num2str(msll_NAEIPbtot(itr)) ', ' num2str(t_NAEIPbtot(itr))]);
+    
+    criterion = 'NAEIPbtur' ; % NAEIPbt, NAEIPbtot, NAEIPbtur, NAEIPat, NAEIPur
+    nip = scale*nt - nt;            % number of inducing points except for test inputs
+    for i = 1:p
+        Xip{i} = repmat(models{i}.Ximean,nip,1) + repmat(std(models{i}.Xi),nip,1).*randn(nip,dtrain);
+    end
+    [mu_NAEIPbtur,s2_NAEIPbtur,t_NAEIPbtur(itr)] = prediction(xtest,models,criterion,nt,nip,Xip);
+    mu_NAEIPbtur = mu_NAEIPbtur*ztrain_std + ztrain_mean;
+    s2_NAEIPbtur = s2_NAEIPbtur*ztrain_std^2;
+    [mse_NAEIPbtur(itr),msll_NAEIPbtur(itr)] = Results(ztest,mu_NAEIPbtur,s2_NAEIPbtur);
+    disp(['NAEIPbtur: ' num2str(mse_NAEIPbtur(itr)) ', ' num2str(msll_NAEIPbtur(itr)) ', ' num2str(t_NAEIPbtur(itr))]);
+
+    criterion = 'NAEIPat' ; % NAEIPbt, NAEIPbtot, NAEIPbtur, NAEIPat, NAEIPur
+    nip = scale*nt;                 % number of inducing points
+    [mu_NAEIPat,s2_NAEIPat,t_NAEIPat(itr)] = prediction(xtest,models,criterion,nt,nip);
+    mu_NAEIPat = mu_NAEIPat*ztrain_std + ztrain_mean;
+    s2_NAEIPat = s2_NAEIPat*ztrain_std^2;
+    [mse_NAEIPat(itr),msll_NAEIPat(itr)] = Results(ztest,mu_NAEIPat,s2_NAEIPat);
+    disp(['NAEIPat: ' num2str(mse_NAEIPat(itr)) ', ' num2str(msll_NAEIPat(itr)) ', ' num2str(t_NAEIPat(itr))]);
+    
+    criterion = 'NAEIPur' ; % NAEIPbt, NAEIPbtot, NAEIPbtur, NAEIPat, NAEIPur
+    nip = scale*nt;                 % number of inducing points
+    for i = 1:p
+        Xip{i} = repmat(models{i}.Ximean,nip,1) + repmat(std(models{i}.Xi),nip,1).*randn(nip,dtrain);
+    end
+    [mu_NAEIPur,s2_NAEIPur,t_NAEIPur(itr)] = prediction(xtest,models,criterion,nt,nip,Xip);
+    mu_NAEIPur = mu_NAEIPur*ztrain_std + ztrain_mean;
+    s2_NAEIPur = s2_NAEIPur*ztrain_std^2;
+    [mse_NAEIPur(itr),msll_NAEIPur(itr)] = Results(ztest,mu_NAEIPur,s2_NAEIPur);
+    disp(['NAEIPur: ' num2str(mse_NAEIPur(itr)) ', ' num2str(msll_NAEIPur(itr)) ', ' num2str(t_NAEIPur(itr))]);
+
+end
+
+
+function [mse,msll] = Results(ztest,mu,s2)
+
+mse = mean((ztest-mu).^2);
+msll = mean(log(2*pi().*s2)./2 + (ztest-mu).^2./(2.*s2));
+
+end
---
+++ prediction.m
@@ -0,0 +1,392 @@
+function [mu,s2,t_predict] = prediction(xtest,models,criterion,varargin)
+% Prediction by NAE-IP
+% Inputs:
+%  Xt: test points
+%  models: p submodels
+%  criterion: NAE-IP with 5 options
+%             'NAEIPbt': nested aggregation of experts, only use test inputs, 'blockwise' extension of NPAE
+%             'NAEIPur':nested aggregation of experts using inducing points with excluding-scheme
+%             'NAEIPbtur':nested aggregation of experts using inducing points with including-scheme
+%             'NAEIPbtot': nested aggregation of experts using test points and other test inputs
+%             'NAEIPat': nested aggregation of experts using random test points
+%  varargin: variables for NAE-IP
+%            nt: number of test inputs processing at once, for NAEIPbt
+%            nip: number of inducing points, for NAEIPbtot and NAEIPat
+%            Xip: p cells of inducing points, for NAEIPur and NAEIPbtur
+% Outputs:
+%  mu: predictive mean
+%  s2: predictive variance
+%  t_predict: computing time for predictions
+%
+% by Ayano NAKAI-KASAI (nakai.ayano@sys.i.kyoto-u.ac.jp).
+% Some parts of functions are based on the implementation by Haitao Liu (H.
+% Liu et al., ICML 2018; https://github.com/LiuHaiTao01/GRBCM)
+
+if nargin==6   % NAEIPur and NAEIPbtur
+    nt = varargin{1};
+    nip = varargin{2};
+    Xip = varargin{3};
+elseif nargin==5   % NAEIPbtot and NAEIPat
+    nt = varargin{1};
+    nip = varargin{2};
+elseif nargin==4   % NAEIPbt
+    nt = varargin{1};
+end   % others
+
+ntest = size(xtest,1);          % number of test points
+p = length(models);             % number of experts
+hyp_lik = models{1}.hyp.lik;
+
+%--- Start prediction ---%
+t1 = cputime;
+if iscell(models{1}.covfunc)
+    covfuncm = models{1}.covfunc;
+    ktt = feval(covfuncm{:},models{1}.hyp.cov,xtest,'diag');    % K**
+else
+    ktt = feval(models{1}.covfunc,models{1}.hyp.cov,xtest,'diag');
+end
+K_invs = inverseKernelMarix_submodels(models);                  % Kii^{-1}
+K_cross = crossKernelMatrix_nestedKG(models);                   % Kij
+
+mu = zeros(ntest,1); s2 = zeros(ntest,1);
+switch criterion  
+    case 'NAEIPbt'
+        for i = 1:p   
+            [mui{i},~] = gp(models{i}.hyp,models{i}.inffunc,models{i}.meanfunc, ...
+                                       models{i}.covfunc,models{i}.likfunc,models{i}.Xi,models{i}.Zi,xtest);
+        end
+        Mu = zeros(ntest,p);
+        for i = 1:p, Mu(:,i) = mui{i}; end
+
+        Numt = ceil(ntest/nt);
+        for i = 1:Numt
+            if i*nt > ntest
+                Idx = (i-1)*nt+1:ntest;  % remainder of test inputs
+            else
+                Idx = (i-1)*nt+1:i*nt;
+            end
+            xt = xtest(Idx,:);
+            tmpMx = Mu(Idx,:);
+            MuA = tmpMx(:);
+            kA = kernelVector_nestedKGbt(xt,models,K_invs);
+            [~,KA_inv] = kernelMatrix_nestedKGbt(xt,models,K_invs,K_cross);
+
+            mu(Idx) = kA'*KA_inv*MuA;
+            s2(Idx) = ktt(Idx) - diag(kA'*KA_inv*kA) + exp(2*hyp_lik);
+        end
+    case 'NAEIPbtot'
+        Numt = ceil(ntest/nt);
+        for i = 1:Numt
+            if i*nt > ntest
+                Idx = (i-1)*nt+1:ntest;  % remainder of test inputs
+            else
+                Idx = (i-1)*nt+1:i*nt;
+            end
+            nu = nip + length(Idx);         % dimension
+            xt = xtest(Idx,:);
+            tmpXt = xtest;
+            tmpXt(Idx,:) = [];              % test points except for xt
+            MuA = zeros(nu*p,1);
+            for j = 1:p   
+                Idx2 = randperm(size(tmpXt,1),nip);
+                Xipot{j} = tmpXt(Idx2,:);   % other test points
+                [MuA((j-1)*nu+1:j*nu),~] = gp(models{j}.hyp,models{j}.inffunc,models{j}.meanfunc, ...
+                                       models{j}.covfunc,models{j}.likfunc,models{j}.Xi,models{j}.Zi,[xt;Xipot{j}]);
+            end
+            kA = kernelVector_nestedKG_NAEIPbtur(xt,models,K_invs,Xipot);
+            [~,KA_inv] = kernelMatrix_nestedKG_NAEIPbtur(xt,models,K_invs,K_cross,Xipot);
+
+            mu(Idx) = kA'*KA_inv*MuA;
+            s2(Idx) = ktt(Idx) - diag(kA'*KA_inv*kA) + exp(2*hyp_lik);
+        end
+    case 'NAEIPbtur'
+        Numt = ceil(ntest/nt);
+        for i = 1:Numt
+            if i*nt > ntest
+                Idx = (i-1)*nt+1:ntest;  % remainder of test inputs
+            else
+                Idx = (i-1)*nt+1:i*nt;
+            end
+            nu = nip + length(Idx);         % dimension
+            xt = xtest(Idx,:);
+            MuA = zeros(nu*p,1);
+            for j = 1:p   
+                [MuA((j-1)*nu+1:j*nu),~] = gp(models{j}.hyp,models{j}.inffunc,models{j}.meanfunc, ...
+                                       models{j}.covfunc,models{j}.likfunc,models{j}.Xi,models{j}.Zi,[xt;Xip{j}]);
+            end
+            kA = kernelVector_nestedKG_NAEIPbtur(xt,models,K_invs,Xip);
+            [~,KA_inv] = kernelMatrix_nestedKG_NAEIPbtur(xt,models,K_invs,K_cross,Xip);
+
+            mu(Idx) = kA'*KA_inv*MuA;
+            s2(Idx) = ktt(Idx) - diag(kA'*KA_inv*kA) + exp(2*hyp_lik);
+        end
+    case 'NAEIPat' % random test inputs
+        nu = nip;                       % dimension
+        MuA = zeros(nu*p,1);
+        for i = 1:p
+            Idx2 = randperm(ntest,nip);
+            Xipat{i} = xtest(Idx2,:);   % arbitrary test points
+            [MuA((i-1)*nu+1:i*nu),~] = gp(models{i}.hyp,models{i}.inffunc,models{i}.meanfunc, ...
+                                   models{i}.covfunc,models{i}.likfunc,models{i}.Xi,models{i}.Zi,Xipat{i});
+        end
+        [~,KA_inv] = kernelMatrix_nestedKG_NAEIPur(models,K_invs,K_cross,Xipat);
+        Numt = ceil(ntest/nt);
+        for i = 1:Numt
+            if i*nt > ntest
+                Idx = (i-1)*nt+1:ntest;  % remainder of test inputs
+            else
+                Idx = (i-1)*nt+1:i*nt;
+            end
+            xt = xtest(Idx,:);
+            kA = kernelVector_nestedKG_NAEIPur(xt,models,K_invs,Xipat);
+
+            mu(Idx) = kA'*KA_inv*MuA;
+            s2(Idx) = ktt(Idx) - diag(kA'*KA_inv*kA) + exp(2*hyp_lik);
+        end
+    case 'NAEIPur'
+        for i = 1:p 
+            [mui{i},~] = gp(models{i}.hyp,models{i}.inffunc,models{i}.meanfunc, ...
+                                       models{i}.covfunc,models{i}.likfunc,models{i}.Xi,models{i}.Zi,Xip{i});
+        end
+        nu = nip;                       % dimension
+        MuA = zeros(nu*p,1);
+        for i = 1:p, MuA((i-1)*nip+1:i*nip) = mui{i}; end
+        [~,KA_inv] = kernelMatrix_nestedKG_NAEIPur(models,K_invs,K_cross,Xip);
+
+        Numt = ceil(ntest/nt);
+        for i = 1:Numt
+            if i*nt > ntest
+                Idx = (i-1)*nt+1:ntest; % remainder of test inputs
+            else
+                Idx = (i-1)*nt+1:i*nt;
+            end
+            xt = xtest(Idx,:);
+            kA = kernelVector_nestedKG_NAEIPur(xt,models,K_invs,Xip);
+
+            mu(Idx) = kA'*KA_inv*MuA;
+            s2(Idx) = ktt(Idx) - diag(kA'*KA_inv*kA) + exp(2*hyp_lik);
+        end
+    otherwise
+        error('Use NAEIPbt, NAEIPbtot, NAEIPbtur, NAEIPat, or NAEIPur.');
+end
+
+t_predict = cputime - t1;
+
+end
+
+
+%%%%%%%%%%%%%%%
+function [K_invs] = inverseKernelMarix_submodels(models)
+% calculate the covariance matrics Ks, the inverse matrics K_invs and the det of matrics K_dets of submodels
+% used for the nestedKG criterion
+p = length(models);
+
+covfunc = models{1}.covfunc; hyp_cov = models{1}.hyp.cov; hyp_lik = models{1}.hyp.lik;
+for i = 1:p
+    if iscell(covfunc)
+        K_Xi = feval(covfunc{:},hyp_cov,models{i}.Xi) + exp(2*hyp_lik)*eye(size(models{i}.Xi,1));
+    else
+        K_Xi = feval(covfunc,hyp_cov,models{i}.Xi) + exp(2*hyp_lik)*eye(size(models{i}.Xi,1));
+    end
+    K_invs{i} = eye(size(models{i}.Xi,1))/K_Xi;
+end
+
+end % end function
+
+
+function K_cross = crossKernelMatrix_nestedKG(models)
+% construct the covariance of training points
+% used for the nestedKG criterion 
+p = length(models);
+K_cross = cell(p,p);
+
+covfunc = models{1}.covfunc; hyp_cov = models{1}.hyp.cov; hyp_lik = models{1}.hyp.lik;
+for i = 1:p 
+    for j = 1:p 
+        if iscell(covfunc)
+            if i == j % self-covariance, should consider noise term
+                K_cross{i}{j} = feval(covfunc{:},hyp_cov,models{i}.Xi,models{j}.Xi) + exp(2*hyp_lik)*eye(size(models{i}.Xi,1));
+            else % cross-covariance
+                K_cross{i}{j} = feval(covfunc{:},hyp_cov,models{i}.Xi,models{j}.Xi);
+            end
+        else
+            if i == j % self-covariance, should consider noise term
+                K_cross{i}{j} = feval(covfunc,hyp_cov,models{i}.Xi,models{j}.Xi) + exp(2*hyp_lik)*eye(size(models{i}.Xi,1));
+            else % cross-covariance
+                K_cross{i}{j} = feval(covfunc,hyp_cov,models{i}.Xi,models{j}.Xi);
+            end
+        end 
+    end
+end
+
+end % end function
+
+
+function kA = kernelVector_nestedKGbt(x,models,K_invs)
+% construct the covariance between test points and training points
+% used for the nestedKG criterion 
+p = length(models);
+nt = size(x,1);
+
+covfunc = models{1}.covfunc; hyp_cov = models{1}.hyp.cov;
+kA = zeros(nt*p,nt);
+for i = 1:p 
+    if iscell(covfunc)
+        k_x_Xi = feval(covfunc{:},hyp_cov,x,models{i}.Xi);
+    else
+        k_x_Xi = feval(covfunc,hyp_cov,x,models{i}.Xi);
+    end
+    kA((i-1)*nt+1:i*nt,:) = k_x_Xi*K_invs{i}*k_x_Xi';
+end
+
+end % end function
+
+
+function [KA,KA_inv] = kernelMatrix_nestedKGbt(x,models,K_invs,K_cross)
+% construct the covariance of training points
+% used for the nestedKG criterion 
+p = length(models);
+nipt = size(x,1);
+
+covfunc = models{1}.covfunc; hyp_cov = models{1}.hyp.cov;
+KA = zeros(nipt*p,nipt*p);
+for i = 1:p 
+    if iscell(covfunc)
+        k_x_Xi = feval(covfunc{:},hyp_cov,x,models{i}.Xi);
+    else
+        k_x_Xi = feval(covfunc,hyp_cov,x,models{i}.Xi);
+    end
+    KA((i-1)*nipt+1:i*nipt,(i-1)*nipt+1:i*nipt) = 0.5.*k_x_Xi*K_invs{i}*k_x_Xi'; % the coef 0.5 is used to ensure KA = KA + KA' along diagonal line
+    for j = i+1:p 
+        if iscell(covfunc)
+            k_Xj_x = feval(covfunc{:},hyp_cov,models{j}.Xi,x);
+        else
+            k_Xj_x = feval(covfunc,hyp_cov,models{j}.Xi,x);
+        end
+        K_Xi_Xj = K_cross{i}{j};
+        KA((i-1)*nipt+1:i*nipt,(j-1)*nipt+1:j*nipt) = k_x_Xi*K_invs{i}*K_Xi_Xj*K_invs{j}*k_Xj_x;
+    end
+end
+% obtain whole KA
+KA = KA + KA';
+
+jitter = 1e-10;
+KA_inv = eye(size(KA,1))/(KA + jitter*eye(size(KA,1)));
+
+end % end function
+
+
+function [KA,KA_inv] = kernelMatrix_nestedKG_NAEIPur(models,K_invs,K_cross,Xip)
+% construct the covariance of inducing points
+% used for the nestedKG criterion 
+p = length(models);
+nip = size(Xip{1},1);
+
+covfunc = models{1}.covfunc; hyp_cov = models{1}.hyp.cov;
+KA = zeros(nip*p,nip*p);
+% obtain an upper triangular matrix to save compting time
+for i = 1:p 
+    if iscell(covfunc)
+        k_xipi_Xi = feval(covfunc{:},hyp_cov,Xip{i},models{i}.Xi);
+    else
+        k_xipi_Xi = feval(covfunc,hyp_cov,Xip{i},models{i}.Xi);
+    end
+    KA((i-1)*nip+1:i*nip,(i-1)*nip+1:i*nip) = 0.5*k_xipi_Xi*K_invs{i}*k_xipi_Xi'; % the coef 0.5 is used to ensure KA = KA + KA' along diagonal line
+    for j = i+1:p 
+        if iscell(covfunc)
+            k_Xj_xipj = feval(covfunc{:},hyp_cov,models{j}.Xi,Xip{j});
+        else
+            k_Xj_xipj = feval(covfunc,hyp_cov,models{j}.Xi,Xip{j});
+        end
+        KA((i-1)*nip+1:i*nip,(j-1)*nip+1:j*nip) = k_xipi_Xi*K_invs{i}*K_cross{i}{j}*K_invs{j}*k_Xj_xipj;
+    end
+end
+% obtain whole KA
+KA = KA + KA';
+
+jitter = 1e-10;
+KA_inv = eye(size(KA,1))/(KA + jitter*eye(nip*p));
+
+end % end function
+
+
+function [KA,KA_inv] = kernelMatrix_nestedKG_NAEIPbtur(x,models,K_invs,K_cross,Xip)
+% construct the covariance of test + other points
+% used for the nestedKG criterion 
+p = length(models);
+nip = size(Xip{1},1)+size(x,1);
+
+covfunc = models{1}.covfunc; hyp_cov = models{1}.hyp.cov;
+KA = zeros(nip*p,nip*p);
+% obtain an upper triangular matrix to save compting time
+for i = 1:p
+    if iscell(covfunc)
+        k_xipi_Xi = feval(covfunc{:},hyp_cov,[x;Xip{i}],models{i}.Xi);
+    else
+        k_xipi_Xi = feval(covfunc,hyp_cov,[x;Xip{i}],models{i}.Xi);
+    end
+    KA((i-1)*nip+1:i*nip,(i-1)*nip+1:i*nip) = 0.5*k_xipi_Xi*K_invs{i}*k_xipi_Xi'; % the coef 0.5 is used to ensure KA = KA + KA' along diagonal line
+    for j = i+1:p
+        if iscell(covfunc)
+            k_Xj_xipj = feval(covfunc{:},hyp_cov,models{j}.Xi,[x;Xip{j}]);
+        else
+            k_Xj_xipj = feval(covfunc,hyp_cov,models{j}.Xi,[x;Xip{j}]);
+        end
+        KA((i-1)*nip+1:i*nip,(j-1)*nip+1:j*nip) = k_xipi_Xi*K_invs{i}*K_cross{i}{j}*K_invs{j}*k_Xj_xipj;
+    end
+end
+% obtain whole KA
+KA = KA + KA';
+
+jitter = 1e-10;
+KA_inv = eye(size(KA,1))/(KA + jitter*eye(nip*p));
+
+end % end function
+
+
+function kA = kernelVector_nestedKG_NAEIPur(x,models,K_invs,Xip)
+% construct the covariance between inducing points and training points
+% used for the nestedKG criterion 
+p = length(models);
+nip = size(Xip{1},1);
+nt = size(x,1);
+
+covfunc = models{1}.covfunc; hyp_cov = models{1}.hyp.cov;
+kA = zeros(nip*p,nt);
+for i = 1:p 
+    if iscell(covfunc)
+        k_xipi_Xi = feval(covfunc{:},hyp_cov,Xip{i},models{i}.Xi);
+        k_Xi_xt = feval(covfunc{:},hyp_cov,models{i}.Xi,x);
+    else
+        k_xipi_Xi = feval(covfunc,hyp_cov,Xip{i},models{i}.Xi);
+        k_Xi_xt = feval(covfunc,hyp_cov,models{i}.Xi,x);
+    end
+    kA((i-1)*nip+1:i*nip,:) = k_xipi_Xi*K_invs{i}*k_Xi_xt;
+end
+
+end % end function
+
+
+function kA = kernelVector_nestedKG_NAEIPbtur(x,models,K_invs,Xip)
+% construct the covariance between test + other points and training points
+% used for the nestedKG criterion 
+p = length(models);
+nip = size(Xip{1},1)+size(x,1);
+nt = size(x,1);
+
+covfunc = models{1}.covfunc; hyp_cov = models{1}.hyp.cov;
+kA = zeros(nip*p,nt);
+for i = 1:p 
+    if iscell(covfunc)
+        k_xipi_Xi = feval(covfunc{:},hyp_cov,[x;Xip{i}],models{i}.Xi);
+        k_Xi_xt = feval(covfunc{:},hyp_cov,models{i}.Xi,x);
+    else
+        k_xipi_Xi = feval(covfunc,hyp_cov,[x;Xip{i}],models{i}.Xi);
+        k_Xi_xt = feval(covfunc,hyp_cov,models{i}.Xi,x);
+    end
+    kA((i-1)*nip+1:i*nip,:) = k_xipi_Xi*K_invs{i}*k_Xi_xt;
+end
+
+end % end function
+
+
